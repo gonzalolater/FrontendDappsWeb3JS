f4c0d2f88b140c7674610f6d4d9c60936fb15dc4
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-10 18:15:17 -0300

    ultimo cambio a route

--- platzi-punks-interface/src/index.js
+++ platzi-punks-interface/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </HashRouter>
   </React.StrictMode>
 );
